2caf9ee356847175792256f58e779a24337523ad
Дадашев Алан <fraindly10@fs.loc>
2026-09-05 20:08:44 +0200

    first commit

---
+++ 05.09.2026.py
@@ -0,0 +1,92 @@
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# total = num1 + num2
+# total2 = num2 - num3
+# print( total2 , total)
+
+# total = int(input())
+# total1 = int(input())
+# num = total // 10
+# num1 = total + total1
+# print(num , num1)
+
+# numbers = [ 1 , 2 , 3 , 2 , 1]
+# print(set(numbers))
+
+# round( num , count) - модуль округления
+# num - число
+# # print(round(4.53850239758934574893 , 2))
+# num = int(input())
+# num1 = int(input())
+# num2 = int(input())
+# num3 = (num1 + num2 + num) / 2
+# total = (num3*(num3 - num)*(num3 - num1)*(num3 - num2) ** 0,25
+# print()
+
+# num = int(input())
+# one = num % 10
+# two = num % 100 // 10
+# three = num % 1000 // 100
+# four = num % 10000 // 1000
+# five = num // 10000
+# print(one + two + three + four + five)
+
+# a = bool(0)
+# print(a)
+#
+# s = None - нечего
+
+
+
+
+# num = False
+# num1 = False
+# total = num1 or num
+# print(total)
+
+# num1 = True
+# num2 = False
+# total = num1 == num2
+# print(total)
+
+# num1 = True
+# num2 = True
+# total = num1 and num2
+# print(total)
+
+# num1 = False
+# num2 = False
+# num3 = True
+# total = ( num1 and num3) or num2
+# print(total)
+# num1 = False
+# num2 = False
+# num3 = True
+# total = num1 == num2 or not num3
+# print(total)
+# Вычислите значение логического
+# выражения: (x↓y)∨¬z, если x = ложь, y = истина, z = истина.
+
+# num1 = False
+# num2 = False
+# num3 = True
+# num4 = False
+# total = (num1 or not num2)  num3
+# print(total)
+
+# имплекация
+# из итены нельзя получить лож
+# изключаеще  или
+# ожин из агрументов истина а другя лож
+
+# a b a +b
+# 0 0 0
+# 1 0 1
+# 0 1 1
+# 1 1 0
+# a = False
+# b = True
+# print(a ^ b)
+# штрих Шеффера
+# отрицание коньюнции
---
+++ 1.py
@@ -0,0 +1,72 @@
+#a = 1
+#print(a)
+#int - цулочисленные тип данных
+#a = 1.
+#print(type(a))
+#type - выводит тип данных обекта
+#a = "'"
+#str - срочный тип данных
+# s = [1,2,"a",[1,2,3,4,]]
+#list - список не связаных по типу
+#float - дробный тип данных
+#bool - 0/1 False/True
+#a = False
+#s = {"key":"value"}
+#dict - словарь ( набор элементов ключ - значение)
+#print(s)
+#s = {1,2,3,4,5,23,2,4,}
+#print(type(s),s)
+#set - набор унекальных эементов
+#s = (1,2,3,4,5,6)
+#tuple - набор неизменяемых элементов
+# float > int
+# s = 10 + 2.0
+# print(type(s))
+# a = True
+# b = 5
+# result = a + b
+# print(result)
+# s = int(value)
+# print(int(10)) ok
+# print(int("10")) ok
+# print(int("10.3")) ne ok
+# a = float("10.3")
+# print(float("10.3"))
+# print(int(float("10.2")))
+# a = int(float("10.2"))
+# print(a)
+# print(a, type(a))
+# a = list("hello")
+# a = list((1,2,3,4))
+# print(a)
+# tuple()
+# dict()
+
+# text = "Python"
+# print(list(text))
+# int_a = int(input())
+# float_a = float(input())
+# str_a = str(input())
+# print(int_a , float_a, str_a)
+
+# nums = [1,2,3,4,5]
+# print(set(nums))
+
+# Даны кортеж coords = (55, 75, 10) и
+# список labels = ["широта", "долгота", "высота"].
+# Выведите на экран первое значение кортежа с
+# первой подписью из списка, а затем второе
+# значение кортежа со второй подписью,
+# используя индексацию по номеру элемента,
+# через разделитель "; ".
+#tuple
+# labels = ["широта", "долгота", "высота"]
+# coords = (55,75,10)
+# print(coords[0],coords[1],coords[2])
+# abc - модуль для числа
+#**
+# print(2 ** (1/2))
+#() - 1
+#** - 2
+#* / // % - 3
+#+ - - 4
