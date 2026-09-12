1b20d22248359426e0fa3da532065cf49f17b654
Дадашев Алан <fraindly10@fs.loc>
2026-09-12 19:42:03 +0200

    чтото

---
+++ 12.09.py
@@ -0,0 +1,58 @@
+# from random import *
+# from math import floor
+# print(randint(1,50))
+# numbers = [1 ,2 , 3, 4, 5]
+# print(shuffle(numbers))
+# print(randint(1,6))
+# a = random()
+# b = 1000 * a
+# s = b ** 0.5
+# print(floor(b))
+# x = 25
+# print(f"{25:b}")
+# print(f"{x:x}")
+# print(f"{x:o}")
+# a = 12
+# b = 15
+# print(f"{a+b:x}")
+# # print(round(uniform(-10,10), 1))
+# from math import *
+# nums = [10 , 20 , 30 , 40 , 50 , 60 ]
+# print(sample(nums, 2))
+
+# from math import *
+# a = randint(1,20)
+# b = randint(1,20)
+# print(sample(nums,2)
+# from random import *
+# from math import *
+# from random import uniform
+# a = uniform(10,20)
+# print(ceil(a))
+# from random import *
+# animals = ['dog', 'cat', 'bird']
+# a = choice(animals)
+# print(a)
+# a = int(input())
+# a = a **2
+# b = oct(a)
+# print(b)
+# print(format(a,"o"))
+# print(f"{a:0}")
+# num1 = int(input(),2)
+# print(bin(num)[2:])
+# # print(bin)
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# num4 = num1 ** 2
+# num5 = num2 * num3
+# num6 = num4 + num5
+# print(bin(num6))
+# a = int(input() ,8)
+# b = int(input() ,8)
+# c = a ** 3
+# f = b // 3
+# e = c + f
+# print(hex(e))
