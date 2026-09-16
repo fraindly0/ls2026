5117d57c4741284a89e2d9e5a9bae131fe73b3e9
Дадашев Алан <fraindly10@fs.loc>
2026-09-16 19:39:13 +0200

    чтото

---
+++ 16.09.py
@@ -0,0 +1,54 @@
+# import math
+# import random
+# from random import randint, randrange
+# a = randrange(1, 99,2 )
+# print(a)
+#
+# A = randint(1,100)
+# B = randint(1,100)
+# K = math.log(A)
+# L = B / 3
+# M = L - K
+# d = math.log(M)
+# print(M)
+# from random import randrange
+# from random import ceil
+# num = randrange(10,100)
+# num1 = num / 7
+# num2 = ceil(num1)
+# print(num2)
+#
+# from random import *
+# from random import sqrt, floor
+# a = random()
+# b = a * 10000
+# c = sqrt(b)
+# print(floot(c), int(c), c // 1)
+
+#задание 7 (работа с изображением)
+
+# 1 - N = 2 ^ i
+# N - коичество цветов в палитре
+# i - глубина цвета
+# формула харди
+# i = log2(N)
+
+# from math import ceil
+# s = 2560 * 5040
+# B = 14175 * 2 **13
+# i = ceil(B / s)
+# N = 2 ** i
+# print(N)
+
+from math import ceil
+s = 7680 * 4320
+b = 4010
+n = 2 ** 16
+D = 9 * 2 ** 33
+i = 16
+l = s * i #сколько памети занимают все изображение
+H = D / l
+amount = int(H)
+print(4010 - int(b/H) * amount)
+
+
